aded1e1c8cb060a23092a6f46f9c292caff0ad04
Jason K. Moore <moorepants@gmail.com>
2015-12-25 14:52:31 -0800

    NumPy 1.8.2 requires a site config file to find the conda openblas.

--- numpy/1.8.2/build.sh
+++ numpy/1.8.2/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+sed -e "s,@PREFIX@,$PREFIX," < $RECIPE_DIR/site-openblas.cfg > site.cfg
+
+if [ ! -f site.cfg ]; then
+    echo "ERROR: *** site.cfg missing ***"
+    exit 1
+fi
+
+$PYTHON setup.py build install
